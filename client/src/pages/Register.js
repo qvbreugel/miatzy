65bb266c2a35f68c8fbf7c1e8ed75c926106c619
qvbreugel <qvbreugel@gmail.com>
2018-12-14 17:19:22 +0100

    Created Basic Register Page & Database Connection

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Header from "../components/Header";
 
-const Register = () => {
-  return (
-    <div>
-      <Header />
-      <h1>Register Here!</h1>
-    </div>
-  );
-};
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      pwd: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      pwd: this.state.pwd
+    };
+
+    //Log Input Data
+    console.log(data);
+
+    fetch("/users/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    })
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error("Bad response from server");
+        }
+        return response.json();
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Register Here!</h1>
+        <form onSubmit={this.handleSubmit} method="POST">
+          <label>Username</label>
+          <input
+            onChange={this.onChange}
+            value={this.state.username}
+            placeholder="Username"
+            name="username"
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            onChange={this.onChange}
+            value={this.state.pwd}
+            name="pwd"
+          />
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Register;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mysql = require("mysql");
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "Localhost",
+  user: "root",
+  database: "miatzy"
+});
+
+function getConnection() {
+  return pool;
+}
+
+router.post("/new", function(req, res, next) {
+  const username = req.body.username;
+  const pwd = req.body.pwd;
+  const connection = getConnection();
+
+  const queryString = "INSERT INTO users (username, pwd) VALUES (?,?)";
+
+  connection.query(queryString, [username, pwd], function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+  });
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
